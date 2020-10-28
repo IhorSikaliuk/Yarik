3cec32b2a2706904cdc9a3db441c9ec890ad2bfb
Ihor Sikaliuk <Ihor.Sikaliuk@udelphi.com>
2020-10-28 17:34:15 +0200

    Init and load

---
+++ Yarik/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Yarik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = "Hello World!";
+            var i = 0;
+            while (i < target.Length)
+            {
+                var symbol = RandomSymbol();
+                Console.Write(symbol);
+                Thread.Sleep(30);
+                if (symbol == target[i])
+                    i++;
+                else
+                    Backspace();
+            }
+
+            Console.ReadKey();
+        }
+
+        static char RandomSymbol()
+        {
+            var symbol = (char) ((new Random().Next()) % 95 + 32);
+            return symbol;
+        }
+
+        static void Backspace()
+        {
+            Console.Write("\b \b");
+        }
+    }
+}
